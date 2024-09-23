515c608ad67be70729a4577e8fc6c7210f550522
HARSHITSETHIYA <117664313+HARSHITSETHIYA@users.noreply.github.com>
2024-09-23 10:44:02 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// we are using node js not browser
+
+// console.log( 3+3); console.log(harshit) //code readability that should be high
+
+ 
+let name ="harshit"
+let age = 32
+let isloggedIn = true
+
+
+// number => 2 to power of 53
+//bigint
+//string  =>""
+//boolean > true/false
+//null = standalone value
+//undefined = value not assigned
+//symbol => used to find uniqueness 
+
+
+//object
+console.log(typeof "");
+
