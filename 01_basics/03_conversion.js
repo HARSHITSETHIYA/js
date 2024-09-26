a4b5a4812f2e44199823e8371900d62838d4795d
HARSHITSETHIYA <117664313+HARSHITSETHIYA@users.noreply.github.com>
2024-09-26 09:40:14 +0000

    conversion

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -18,7 +18,21 @@ console.log(booleanIsLoggedIn);
 // "" = false
 // "hs " = true
  
-let someNumber = 33 
+// let someNumber = 33 
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+//operations
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "harshit "
+
+
+let str3 = str1+str2
+console.log(str3);
+ 
