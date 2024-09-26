9a311251cf7a7752862aa4c50b57fe492d11466f
HARSHITSETHIYA <117664313+HARSHITSETHIYA@users.noreply.github.com>
2024-09-26 09:59:36 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,37 @@
+//  primitive
+
+// 7 types : string ,number , boolean , null, undefined, symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+const isloggedIn = false
+const outsidetemp = null
+let userEMail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+const bigNumber = 13564164514255545n
+
+
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+// const heros = ["shaktiman","nagraj","doga"]
+// let myObj ={
+//     name:harshit,
+//     age:22,
+// }
+
+const myfunction= function(){
+    console.log("hello world");
+    
+}
+console.log(typeof bigNumber);
